d9ccb793728255e39169d9ae9903e0e45a4af5a3
Erik Trom <erik.trom@gmail.com>
2015-09-15 01:53:41 -0700

    add index hooks

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 /* jshint node: true */
 'use strict';
 
+var HapiFastboot = require('./lib/commands/hapi-fastboot');
+var HapiFastbootBuild = require('./lib/commands/hapi-fastboot-build');
+
 module.exports = {
   name: 'ember-cli-hapi-fastboot',
+
+  includedCommands: function includedCommands() {
+    return {
+      'hapi-fastboot': HapiFastboot,
+      'hapi-fastboot:build': HapiFastbootBuild
+    };
+  },
+
+  contentFor: function(type) {
+    // do nothing unless running `ember fastboot` command
+    if (!process.env.EMBER_CLI_HAPI_FASTBOOT) { return; }
+
+    if (type === 'body') {
+      return "<!-- EMBER_CLI_HAPI_FASTBOOT_BODY -->";
+    }
+
+    if (type === 'head') {
+      return "<!-- EMBER_CLI_HAPI_FASTBOOT_TITLE -->";
+    }
+
+    if (type === 'vendor-prefix') {
+      return '// Added from ember-cli-hapi-fastboot \n' +
+             'EmberENV.FEATURES = EmberENV.FEATURES || {};\n' +
+             'EmberENV.FEATURES["ember-application-visit"] = true;\n';
+    }
+  },
+
+  included: function() {
+    if (process.env.EMBER_CLI_HAPI_FASTBOOT) {
+      this.app.options.storeConfigInMeta = false;
+      process.env.EMBER_CLI_HAPI_FASTBOOT_APP_NAME = this.app.name;
+    }
+  }
 };
